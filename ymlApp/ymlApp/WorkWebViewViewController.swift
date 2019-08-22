7652022030768852860b7fb03f2444a0a6c6f72c
Nidhishree <nidhishree@Ranjits-Mac-mini.local>
2019-08-22 20:35:45 +0530

    working on webview

--- ymlApp/ymlApp/WorkWebViewViewController.swift
+++ ymlApp/ymlApp/WorkWebViewViewController.swift
@@ -7,24 +7,22 @@
 //
 
 import UIKit
+import WebKit
 
-class WorkWebViewViewController: UIViewController {
-
+class WorkWebViewViewController: UIViewController,WKNavigationDelegate {
+    var webView: WKWebView!
+    var url: URL!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://ioscreator.com")!
+        webView.load(URLRequest(url: url))
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
     }
-    */
-
+   
 }
