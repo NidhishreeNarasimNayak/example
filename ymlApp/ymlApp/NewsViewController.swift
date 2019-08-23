1a2de3f73339275f92747ca6f3438457c213b5cc
Nidhishree <nidhishree@Ranjits-Mac-mini.local>
2019-08-23 16:58:31 +0530

    added image in new screen

--- ymlApp/ymlApp/NewsViewController.swift
+++ ymlApp/ymlApp/NewsViewController.swift
@@ -25,9 +25,9 @@ class NewsViewController: UIViewController {
     }
     
     func newsScreenObject() {
-        let featuredobject = workElements(imagename: "northFace", headertitle: "THE NORTH FACE", description: "How The North Face redefined loyalty to embrace the great outdoors.",url: "https://ymedialabs.com/project/the-north-face")
-        let designObject = workElements(imagename: "cloverGo", headertitle: "CLOVER", description: "How CLover Go has become an open ecosystem for point-of-sale payments.",url: "https://ymedialabs.com/project/clover")
-        let eBooksObject = workElements(imagename: "creditOne", headertitle: "CREDIT ONE", description: "How Credit One has become America's fastest growing credit card issuer.",url: "https://ymedialabs.com/project/credit-one")
+        let featuredobject = workElements(imagename: "featured1", headertitle: "THE NORTH FACE", description: "How The North Face redefined loyalty to embrace the great outdoors.",url: "https://ymedialabs.com/project/the-north-face")
+        let designObject = workElements(imagename: "featured2", headertitle: "CLOVER", description: "How CLover Go has become an open ecosystem for point-of-sale payments.",url: "https://ymedialabs.com/project/clover")
+        let eBooksObject = workElements(imagename: "featured3", headertitle: "CREDIT ONE", description: "How Credit One has become America's fastest growing credit card issuer.",url: "https://ymedialabs.com/project/credit-one")
         allElements.append(featuredobject)
         allElements.append(designObject)
         allElements.append(eBooksObject)
@@ -41,6 +41,7 @@ extension NewsViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "newsCell", for: indexPath) as? NewsTableViewCell
         let element = allElements[indexPath.row]
+        cell?.newsScreenImageView.image = UIImage(named: element.imagename)
         cell?.newsScreenTitleLabel.text = element.headertitle
         cell?.newsScreenDescriptionLabel.text = element.description
         return cell ?? UITableViewCell()
